dbbaf37cec2bf7277d83feb69ec55c81751ce1fd
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-03-27 14:13:19 -0500

    XDDD

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello Word")
+/*
+let username = "Akari";
+let age = 23;
+let hashobbies = false;
+let points = [10,20,30]
+let user = {
+    name: "Akari",
+    lastname: "jhomen"
+}
+*/
+const PI = 3.1415
+
+const age = 30                 
